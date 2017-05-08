b315e83991d0113fc5929ae65c504c947205e8a0
Marek Schmidt <maschmid@redhat.com>
2017-05-08 21:42:30 +0200

    install

--- install.sh
+++ install.sh
@@ -9,10 +9,12 @@ oc expose service hystrix-dashboard --hostname=hystrix.swafel.com --port=9002
 for i in api catalog inventory shop
 do
   pushd $i-service
+  mvn clean
   oc new-app --docker-image=registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift:latest . --name=$i
   oc start-build $i --from-dir=.
   oc env dc $i ZIPKIN_SERVER_URL=http://zipkin:9411
   oc patch service $i -p '{"spec":{"ports":[{"name":"80-tcp","port":80,"targetPort":8080}]}}'
+  oc expose service $i --hostname=$i.swafel.com --port=8080
   popd
 done
 
